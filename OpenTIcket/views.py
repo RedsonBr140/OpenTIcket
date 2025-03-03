daac5affd22a89470d2fc24ad839baed1d447096
Redson dos Santos <redson@riseup.net>
2025-03-02 22:06:30 -0300

    refactor: Rewrote the main page

--- OpenTIcket/views.py
+++ OpenTIcket/views.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from tickets.models import Ticket
+
+
 @login_required
 def home(request):
-    return render(request, 'index.html')
+    tickets = Ticket.objects.all()
+
+    tickets = tickets.filter(author=request.user)
+    return render(request, 'index.html', {'tickets': tickets})
