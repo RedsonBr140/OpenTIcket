4d7239de8fe9e110e9747bb3d1e1339fe4ef0b1a
Redson <redson@riseup.net>
2025-03-15 16:55:24 -0300

    fix: Correct the spelling of 'OpenTicket' in Celery configuration

--- OpenTIcket/celery.py
+++ OpenTIcket/celery.py
@@ -3,9 +3,9 @@ import os
 from celery import Celery
 from django.conf import settings
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "OpenTicket.settings_dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "OpenTIcket.settings_dev")
 
-app = Celery("openticket")
+app = Celery("OpenTIcket")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
